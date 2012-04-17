99851cff019169ee4298b62f40c4a38843826f14
Vincent Dupain <vdupain@gmail.com>
2012-04-17 14:47:31 +0200

    Adding River

---
+++ src/main/java/com/mycompany/river/MyDummyRiverModule.java
@@ -0,0 +1,14 @@
+package com.mycompany.river;
+
+import org.elasticsearch.common.inject.AbstractModule;
+import org.elasticsearch.river.River;
+import org.elasticsearch.river.dummy.DummyRiver;
+
+public class MyDummyRiverModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+		bind(River.class).to(DummyRiver.class).asEagerSingleton();
+	}
+
+}
